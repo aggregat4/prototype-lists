c00a3818ea3c8be7f06fdbbd78bfb355aa50879c
Boris Terzic <boris@terzic.be>
2025-10-18 21:30:30 +0200

    Move playwright tests to use a web server as well

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,17 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import type { PlaywrightTestConfig } from "@playwright/test";
+
+const baseURL = process.env.PLAYWRIGHT_TEST_BASE_URL ?? "http://127.0.0.1:8000";
 
 const config: PlaywrightTestConfig = {
+  use: {
+    baseURL,
+  },
   projects: [
     {
-      name: 'chromium',
-      use: { browserName: 'chromium' }
-    }
-  ]
+      name: "chromium",
+      use: { browserName: "chromium" },
+    },
+  ],
 };
 
 export default config;
